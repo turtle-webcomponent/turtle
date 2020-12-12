c2a69ff8af64b6cd1b876427c165e376b556a7eb
/home/thalisson-melo/Documents/dotfiles <tallisonmelo@hotmail.com>
2020-12-12 00:46:58 -0300

    Make turutle variables private

--- turtle.js
+++ turtle.js
@@ -1,22 +1,27 @@
 class Turtle {
+  #canvas;
+  #lastRender;
+  #move;
+  #draw_moves;
+  #position;
+  #angle;
+  #line_width;
+  #speed;
+  #context;
 
   //Add turtle picture
   constructor(canvas) {
-    this.lastRender = 0
-    this.position = new Point(0,0)
-    this.color = 'black'
+    this.#lastRender = 0
 
-    this.distance = 0
-    this.move = []
-    this.draw_moves = []
-    this.new_position = {x:0, y:0}
-    this.angle = 0
-    this.line_width = 2
-    this.speed = 1
+    this.#move = []
+    this.#draw_moves = []
+    this.#position = {x:0, y:0}
+    this.#angle = 0
+    this.#line_width = 1
+    this.#speed = 1
 
-    // Make this private
-    this.canvas = canvas;
-    this.context = this.canvas.getContext("2d")
+    this.#canvas = canvas;
+    this.#context = this.#canvas.getContext("2d")
 
     // binds
     this.turtle = this.turtle.bind(this)
@@ -25,8 +30,10 @@ class Turtle {
     this.forward = this.forward.bind(this)
     this.right = this.right.bind(this)
     this.left = this.left.bind(this)
+    this.rotate = this.rotate.bind(this)
+    this.angle_in_degrees = this.angle_in_degrees.bind(this)
 
-    this.context.translate(this.canvas.width * 0.5, this.canvas.height * 0.5);
+    this.#context.translate(this.#canvas.width * 0.5, this.#canvas.height * 0.5);
   }
 
   run() {
@@ -34,77 +41,77 @@ class Turtle {
   }
 
   turtle(timestamp) {
-    var progress = timestamp - this.lastRender
+    var progress = timestamp - this.#lastRender
 
     this.update(progress)
     this.draw()
 
-    this.lastRender = timestamp
+    this.#lastRender = timestamp
     window.requestAnimationFrame(this.turtle)
   }
 
   update(progress) {
-    if(this.move) {
-      if(this.move.length) {
-        if (this.move[0].type === 'reposition') this.reposition_move()
-        else if (this.move[0].type === 'move') this.simple_move(progress)
-        else if (this.move[0].type === 'circle') this.circle_move()
-        else if (this.move[0].type === 'rectangle') this.rectangle_move()
-        else if(this.move[0].type === 'change_color') {
-          this.draw_moves.push({type: this.move[0].type, color: this.move[0].color})
+    if(this.#move) {
+      if(this.#move.length) {
+        if (this.#move[0].type === 'reposition') this.reposition_move()
+        else if (this.#move[0].type === 'move') this.simple_move(progress)
+        else if (this.#move[0].type === 'circle') this.circle_move()
+        else if (this.#move[0].type === 'rectangle') this.rectangle_move()
+        else if(this.#move[0].type === 'change_color') {
+          this.#draw_moves.push({type: this.#move[0].type, color: this.#move[0].color})
         }
 
-        if (this.move[0].type !== 'move' || Math.round(this.move[0].distance) === 0) this.move.splice(0, 1)
+        if (this.#move[0].type !== 'move' || Math.round(this.#move[0].distance) === 0) this.#move.splice(0, 1)
       }
     }
   }
 
   reposition_move() {
-    this.draw_moves.push({ x: this.move[0].x, y: this.move[0].y, type: this.move[0].type })
-    this.new_position = { x: this.move[0].x, y: this.move[0].y }
+    this.#draw_moves.push({ x: this.#move[0].x, y: this.#move[0].y, type: this.#move[0].type })
+    this.#position = { x: this.#move[0].x, y: this.#move[0].y }
   }
 
   simple_move(fps) {
     var displacement = null;
 
-    if (Math.abs(this.move[0].distance) < this.speed/10 * fps) {
-      displacement = this.move[0].distance;
+    if (Math.abs(this.#move[0].distance) < this.#speed/10 * fps) {
+      displacement = this.#move[0].distance;
     }
     else {
-      if(this.move[0].distance > 0) {
-        displacement = Math.round(this.speed/10 * fps)
+      if(this.#move[0].distance > 0) {
+        displacement = Math.round(this.#speed/10 * fps)
       }
       else {
-        displacement = Math.round(this.speed/10 * fps) * -1
+        displacement = Math.round(this.#speed/10 * fps) * -1
       }
     }
 
-    this.draw_moves.push({ x: this.new_position.x, y: this.new_position.y, type: this.move[0].type })
+    this.#draw_moves.push({ x: this.#position.x, y: this.#position.y, type: this.#move[0].type })
 
-    this.new_position = this.position.rotate(this.move[0].angle, displacement, this.new_position)
-    this.move[0].distance -= displacement
+    this.#position = this.rotate(this.#move[0].angle, displacement, this.#position)
+    this.#move[0].distance -= displacement
 
-    if(this.move[0].distance === 0) {
-      this.draw_moves.push({ x: this.new_position.x, y: this.new_position.y, type: this.move[0].type })
+    if(this.#move[0].distance === 0) {
+      this.#draw_moves.push({ x: this.#position.x, y: this.#position.y, type: this.#move[0].type })
     }
   }
 
   circle_move() {
-    this.draw_moves.push({ x: this.new_position.x, y: this.new_position.y, type: this.move[0].type, radius: this.move[0].radius })
+    this.#draw_moves.push({ x: this.#position.x, y: this.#position.y, type: this.#move[0].type, radius: this.#move[0].radius })
   }
 
   rectangle_move() {
-    this.draw_moves.push({
-      x: this.new_position.x,
-      y: this.new_position.y,
-      width: this.move[0].width,
-      height: this.move[0].height,
-      type: this.move[0].type
+    this.#draw_moves.push({
+      x: this.#position.x,
+      y: this.#position.y,
+      width: this.#move[0].width,
+      height: this.#move[0].height,
+      type: this.#move[0].type
     })
   }
 
   forward(value) {
-    this.move.push({distance: value, angle: this.angle, type: 'move'})
+    this.#move.push({distance: value, angle: this.#angle, type: 'move'})
   }
 
   backward(value) {
@@ -112,67 +119,62 @@ class Turtle {
   }
 
   circle(radius) {
-    this.move.push({radius: radius, type: 'circle'})
+    this.#move.push({radius: radius, type: 'circle'})
   }
 
   rectangle(width, height) {
-    this.move.push({width: width, height: height, type: 'rectangle'})
+    this.#move.push({width: width, height: height, type: 'rectangle'})
   }
 
   right(value) {
-    this.angle = value + this.angle
+    this.#angle = value + this.#angle
   }
 
   left(value) {
-    this.angle = 360 - value + this.angle
+    this.#angle = 360 - value + this.#angle
   }
 
   set_line_color(color) {
-    this.move.push({color: color, type: 'change_color'})
+    this.#move.push({color: color, type: 'change_color'})
   }
 
   set_position(x, y) {
-    this.move.push({x: x, y: y, type: 'reposition'})
+    this.#move.push({x: x, y: y, type: 'reposition'})
   }
 
   set_line_width(value) {
-    this.line_width = value;
+    this.#line_width = value;
+  }
+
+  speed(value) {
+    this.#speed = value
   }
 
   draw() {
-    this.context.lineWidth = this.line_width
-    if(this.draw_moves.length) {
-      if(this.draw_moves[0].type === 'reposition') {
-        this.context.moveTo(this.draw_moves[0].x, this.draw_moves[0].y)
+    this.#context.lineWidth = this.#line_width
+    if(this.#draw_moves.length) {
+      if(this.#draw_moves[0].type === 'reposition') {
+        this.#context.moveTo(this.#draw_moves[0].x, this.#draw_moves[0].y)
       }
-      else if(this.draw_moves[0].type === 'move') {
-        this.context.lineTo(this.draw_moves[0].x, this.draw_moves[0].y)
+      else if(this.#draw_moves[0].type === 'move') {
+        this.#context.lineTo(this.#draw_moves[0].x, this.#draw_moves[0].y)
       }
-      else if(this.draw_moves[0].type === 'circle') {
-        this.context.beginPath();
-        this.context.arc(this.draw_moves[0].x,this.draw_moves[0].y,this.draw_moves[0].radius,0,2*Math.PI);
+      else if(this.#draw_moves[0].type === 'circle') {
+        this.#context.beginPath();
+        this.#context.arc(this.#draw_moves[0].x,this.#draw_moves[0].y,this.#draw_moves[0].radius,0,2*Math.PI);
       }
-      else if(this.draw_moves[0].type === 'rectangle') {
-        this.context.beginPath();
-        this.context.rect(this.draw_moves[0].x, this.draw_moves[0].y, this.draw_moves[0].width, this.draw_moves[0].height);
+      else if(this.#draw_moves[0].type === 'rectangle') {
+        this.#context.beginPath();
+        this.#context.rect(this.#draw_moves[0].x, this.#draw_moves[0].y, this.#draw_moves[0].width, this.#draw_moves[0].height);
       }
-      else if(this.draw_moves[0].type === 'change_color') {
-        this.context.beginPath();
-        this.context.strokeStyle = this.draw_moves[0].color;
+      else if(this.#draw_moves[0].type === 'change_color') {
+        this.#context.beginPath();
+        this.#context.strokeStyle = this.#draw_moves[0].color;
       }
-      this.context.stroke();
+      this.#context.stroke();
 
-      this.draw_moves.splice(0, 1);
+      this.#draw_moves.splice(0, 1);
     }
-  }
-}
-
-class Point {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-    this.rotate = this.rotate.bind(this)
-    this.angle_in_degrees = this.angle_in_degrees.bind(this)
   }
 
   rotate(angle, distance, position) {
@@ -184,6 +186,13 @@ class Point {
 
    angle_in_degrees(angle) {
     return angle * Math.PI / 180
+  }
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
   }
 }
 

d5153f888471d0c760694c2672f3121d2e5301cf
Thalisson Melo <tallisonmelo@hotmail.com>
2021-05-21 18:35:33 -0300

    Make refactors on turtle

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ class TurtleComponent extends HTMLElement {
   #backgroundCanvas
   #backgroundCanvasContext
   #parentDiv
-  #image
+  #moveSprite
+  #idleSprite
 
   static get observedAttributes() {
     return ['width', 'height'];
@@ -14,16 +15,14 @@ class TurtleComponent extends HTMLElement {
     this.#parentDiv = document.createElement("div")
     this.#backgroundCanvas = document.createElement("canvas");
 
-    this.turtles = [];
-
     this.#parentDiv.appendChild(this.#backgroundCanvas);
 
-    this.#backgroundCanvasContext = this.#backgroundCanvas.getContext("2d")
+    this.#moveSprite = new Image();
+    this.#moveSprite.src = 'turtle.png';
+    this.#idleSprite = new Image();
+    this.#idleSprite.src = 'idle_turtle.png';
 
-    this.#image = new Image();
-    this.#image.src = 'turtle.png';
-    this.image2 = new Image();
-    this.image2.src = 'idle_turtle.png';
+    this.#backgroundCanvasContext = this.#backgroundCanvas.getContext("2d")
   }
 
   get width() {
@@ -50,6 +49,14 @@ class TurtleComponent extends HTMLElement {
     this.setAttribute('canvas-style', value);
   }
 
+  get spriteScale() {
+    return this.getAttribute('sprite-scale');
+  }
+
+  set spriteScale(value) {
+    this.setAttribute('sprite-scale', value);
+  }
+
   connectedCallback() {
     if (!this.width) {
       this.rating = 300;
@@ -59,6 +66,9 @@ class TurtleComponent extends HTMLElement {
     }
     if (!this.canvasStyle) {
       this.maxRating = "border: solid 1px black;";
+    }
+    if (!this.spriteScale) {
+      this.spriteScale = 1;
     }
 
     this.initializeCanvas()
@@ -88,6 +98,15 @@ class TurtleComponent extends HTMLElement {
         case 'canvas-style':
           this.canvasStyle = newVal
           break;
+        case 'idle-sprite':
+          this.#idleSprite.src = newVal
+          break;
+        case 'moving-sprite':
+          this.#moveSprite.src = newVal
+          break;
+        case 'sprite-scale':
+          this.spriteScale = newVal
+          break;
       }
     }
   }
@@ -98,9 +117,7 @@ class TurtleComponent extends HTMLElement {
     }
   }
 
-  getTurtle() {
-    console.log(this.width, this.height)
-
+  buildForwardCanvas() {
     let foregroundCanvas = document.createElement("canvas");
     foregroundCanvas.style = "position: absolute;"
     this.#parentDiv.appendChild(foregroundCanvas);
@@ -111,10 +128,26 @@ class TurtleComponent extends HTMLElement {
     foregroundCanvas.width = this.width
     foregroundCanvas.height = this.height
 
-    foregroundCanvasContext.translate(foregroundCanvas.width * 0.5 - 50, foregroundCanvas.height * 0.5 - 50);
+    console.log("values test", ((this.#moveSprite.width/8)*this.spriteScale))
+    console.log("values test", (this.#idleSprite.height*this.spriteScale))
+    foregroundCanvasContext.translate(
+      foregroundCanvas.width * 0.5 - (((this.#moveSprite.width/8)*this.spriteScale)/2),
+      foregroundCanvas.height * 0.5 - ((this.#idleSprite.height*this.spriteScale)/2)
+    );
 
-    var turtle = new Turtle(this.#backgroundCanvasContext, foregroundCanvasContext,this.#image, this.image2, this.width, this.height)
-    this.turtles.push(turtle)
+    return foregroundCanvasContext
+  }
+
+  getTurtle() {
+    var turtle = new Turtle(
+      this.#backgroundCanvasContext,
+      this.buildForwardCanvas(),
+      this.#moveSprite,
+      this.#idleSprite,
+      this.spriteScale,
+      this.width,
+      this.height
+    )
 
     this.update(turtle)
 
@@ -123,8 +156,6 @@ class TurtleComponent extends HTMLElement {
 }
 
 customElements.define('x-turtle', TurtleComponent);
-
-const BIGGER_POSITION = 2*(10**8)
 
 class Turtle {
   #position
@@ -135,7 +166,7 @@ class Turtle {
   #backgroundCanvas
   #foregroundCanvas
 
-  constructor(backgroundCanvas, foregroundCanvas, image, image2, width, height) {
+  constructor(backgroundCanvas, foregroundCanvas, spriteIdle, spriteMoving, spriteScale, width, height) {
     this.#position = { x: 0, y: 0 }
     this.spritePosition = { x: 0, y: 0}
     this.#angle = 0
@@ -144,10 +175,11 @@ class Turtle {
     this.width = width;
     this.height = height;
 
+
     this.#backgroundCanvas = backgroundCanvas;
     this.#foregroundCanvas = foregroundCanvas;
-    this.sprite = new Sprite(1, 8, image, foregroundCanvas, true)
-    this.sprite2 = new Sprite(1, 10, image2, foregroundCanvas, false)
+    this.idleSprite = new Sprite(1, 8, spriteIdle, foregroundCanvas, spriteScale)
+    this.moveSprite = new Sprite(1, 10, spriteMoving, foregroundCanvas, spriteScale)
     this.#actions = []
 
     console.log(backgroundCanvas)
@@ -181,6 +213,8 @@ class Turtle {
   }
 
   forward(distance) {
+    console.log('width', this.#backgroundCanvas.canvas.width)
+    console.log('width', this.#backgroundCanvas.canvas)
     this.#actions.push({action: 'forwardAction', parameters: [distance]})
   }
 
@@ -288,9 +322,7 @@ class Turtle {
     console.log("sprite pos", this.spritePosition)
     console.log("pos", this.#position)
 
-    this.#foregroundCanvas.translate(this.spritePosition.x + 50, this.spritePosition.y + 50);
-    this.#foregroundCanvas.rotate(angleInDegrees(value));
-    this.#foregroundCanvas.translate(-this.spritePosition.x - 50, -this.spritePosition.y - 50);
+    this.rotateForegroundCanvas('right', value)
   }
 
   left(value) {
@@ -300,9 +332,23 @@ class Turtle {
   async leftAction(value) {
     this.#angle = 360 - parseInt(value) + this.#angle
 
-    this.#foregroundCanvas.translate(this.spritePosition.x + 50, this.spritePosition.y + 50);
-    this.#foregroundCanvas.rotate(angleInDegrees(-value))
-    this.#foregroundCanvas.translate(-this.spritePosition.x - 50, -this.spritePosition.y - 50);
+    this.rotateForegroundCanvas('left', value)
+  }
+
+  rotateForegroundCanvas(direction, value) {
+    if(direction == 'left')
+      value *= -1
+
+    let idleSpriteCenterOffset = this.idleSprite.getCenterOffset()
+    let movingSpriteCenterOffset = this.moveSprite.getCenterOffset()
+
+    console.log("offset", idleSpriteCenterOffset, ((this.moveSprite.width/8)/2))
+    console.log("offset", movingSpriteCenterOffset)
+    console.log("value", value)
+
+    this.#foregroundCanvas.translate(this.spritePosition.x + idleSpriteCenterOffset.width, this.spritePosition.y + idleSpriteCenterOffset.height);
+    this.#foregroundCanvas.rotate(angleInDegrees(value))
+    this.#foregroundCanvas.translate(-this.spritePosition.x - movingSpriteCenterOffset.width, -this.spritePosition.y - movingSpriteCenterOffset.height);
   }
 
   setPosition(x, y) {
@@ -332,10 +378,10 @@ class Turtle {
     );
 
     if(this.moving) {
-      await this.sprite.run(this.spritePosition, this.#angle)
+      await this.idleSprite.run(this.spritePosition)
     }
     else {
-      await this.sprite2.run(this.spritePosition, this.#angle)
+      await this.moveSprite.run(this.spritePosition)
     }
 
     var t1 = performance.now()
@@ -360,8 +406,7 @@ function postionWithAngle(angle, distance, position) {
 }
 
 class Sprite {
-  constructor(rows, columns, sprite, canvas, a) {
-    this.rows = rows;
+  constructor(rows, columns, sprite, canvas, spriteScale) {
     this.columns = columns;
     this.sprite = sprite;
     this.frameWidth = sprite.width / columns;
@@ -369,11 +414,11 @@ class Sprite {
     this.currentFrame = 0;
     this.maxFrame = columns * rows - 1;
     this.canvas = canvas;
-    this.position = {x:-50, y:-50}
-    this.a = a
+    this.spriteScale = {width: this.frameWidth*spriteScale, height: this.frameHeight*spriteScale};
+    console.log("sprite scale", spriteScale)
   }
 
-  async run(position, angle) {
+  async run(position) {
     this.currentFrame++;
 
     if (this.currentFrame > this.maxFrame){
@@ -391,9 +436,13 @@ class Sprite {
       this.frameHeight,
       position.x,
       position.y,
-      100,
-      100);
+      this.spriteScale.width,
+      this.spriteScale.height);
 
     await sleep(1000/this.maxFrame)
+  }
+
+  getCenterOffset() {
+    return { width: (this.spriteScale.width/2), height: (this.spriteScale.height/2) }
   }
 }

4a08d03652c506c9d0aa015ca67fd80cba14fb00
Guilherme Silva <guilherme.francais@gmail.com>
2021-08-20 19:24:05 -0300

    Creates node server to modularize turtle code in specific files

---
+++ server.js
@@ -0,0 +1,30 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+http.createServer(function (request, response) {
+
+  var filePath = '.' + request.url;
+  if (filePath == './') {
+    filePath = './index.html';
+  }
+
+  var extname = String(path.extname(filePath)).toLowerCase();
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+  };
+
+  var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function (error, content) {
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(content, 'utf-8');
+  });
+
+}).listen(8000);
+console.log('Server running at http://127.0.0.1:8000/');
